3fa3dc24ea3cd6a18f66ca2988b4c6bb70b372cf
menmaa <31330913+menmaa@users.noreply.github.com>
2019-02-01 07:38:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# cu-kill-feed
+
+A tera-proxy module. Restores the pre-patch 75 guild kill/death feed in Civil Unrest: Velika map. Supports auto update functionality with Caali's proxy.
+
+If you like to offer me a few drinks -> [PayPal](https://www.paypal.me/menmaaa)
+
+### Preview
+
+![Preview Image](https://raw.githubusercontent.com/menmaa/cu-kill-feed/master/preview.png)
---
+++ index.js
@@ -0,0 +1,103 @@
+module.exports = function CUKillFeed(mod) {
+	
+	if(mod.proxyAuthor == "caali") {
+		mod.dispatch.protocol.messages.set('S_USER_DEATH', new Map().set(1, [['name', 'offset'], ['killerName', 'offset'], ['type', 'byte'], ['name', 'string'], ['killerName', 'string']]));
+	} else {
+		const { protocol } = require('tera-data-parser');
+		protocol.messages.set('S_USER_DEATH', new Map().set(1, [['name', 'offset'], ['killerName', 'offset'], ['type', 'byte'], ['name', 'string'], ['killerName', 'string']]));
+	}
+	
+	let loaded = false;
+	let hooks = [];
+	let guildMap = new Map();
+	
+	mod.hook('S_LOAD_TOPO', 3, (event) => {
+		if(event.zone === 152) load();
+		else unload();
+	});
+	
+	function load() {
+		if(loaded) return;
+		
+		hook('S_RETURN_TO_LOBBY', 1, () => {
+			unload();
+		});
+		
+		hook('S_USER_PAPERDOLL_INFO', 5, (event) => {
+			if(!guildMap.has(event.name)) {
+				guildMap.set(event.name, event.guild);
+			}
+		});
+		
+		hook('S_USER_DEATH', 1, (event) => {
+			if(event.killerName.length > 0) {
+				sendSystemMessage(event);
+			}
+		});
+		
+		hook('S_SPAWN_USER', 13, (event) => {
+			if(!guildMap.has(event.name)) {
+				guildMap.set(event.name, event.guild);
+			}
+		});
+		
+		loaded = true;
+	}
+	
+	function unload() {
+		if(!loaded) return;
+		
+		for(let h of hooks) {
+			mod.unhook(h);
+		}
+		
+		hooks = [];
+		guildMap.clear();
+		loaded = false;
+	}
+	
+	function sendSystemMessage(e) {
+		switch(e.type) {
+			case 0: {
+				if(!guildMap.has(e.name)) {
+					mod.hookOnce('S_USER_PAPERDOLL_INFO', 5, (event) => {
+						guildMap.set(event.name, event.guild);
+						sendSystemMessage(e);
+						return false;
+					});
+					mod.send('C_REQUEST_USER_PAPERDOLL_INFO', 1, { name: e.name });
+				} else {
+					let message = mod.buildSystemMessage('SMT_CITYWAR_GUILD_KILL', {
+						Name1: e.killerName,
+						GuildName2: guildMap.get(e.name),
+						Name2: e.name
+					});
+					mod.send('S_SYSTEM_MESSAGE', 1, { message });
+				}
+				break;
+			}
+			case 1: {
+				if(!guildMap.has(e.killerName)) {
+					mod.hookOnce('S_USER_PAPERDOLL_INFO', 5, (event) => {
+						guildMap.set(event.name, event.guild);
+						sendSystemMessage(e);
+						return false;
+					});
+					mod.send('C_REQUEST_USER_PAPERDOLL_INFO', 1, { name: e.killerName });
+				} else {
+					let message = mod.buildSystemMessage('SMT_CITYWAR_GUILD_DEATH', {
+						Name1: e.name,
+						GuildName2: guildMap.get(e.killerName),
+						Name2: e.killerName
+					});
+					mod.send('S_SYSTEM_MESSAGE', 1, { message });
+				}
+				break;
+			}
+		}
+	}
+	
+	function hook() {
+		hooks.push(mod.hook(...arguments));
+	}
+}
